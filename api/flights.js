f82a92f6a0e73eb4157564eb8afec9335fcfca8e
vvishwak666-lgtm <vvishwak666@gmail.com>
2026-08-18 21:30:37 +1200

    Update flights.js

--- api/flights.js
+++ api/flights.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   url.searchParams.set("access_key", apiKey);
   url.searchParams.set(airportParam, "AKL");
   url.searchParams.set("airline_iata", "NZ");
-  url.searchParams.set("limit", "20");
+  url.searchParams.set("limit", "100");
 
   try {
     const upstream = await fetch(url.toString());
